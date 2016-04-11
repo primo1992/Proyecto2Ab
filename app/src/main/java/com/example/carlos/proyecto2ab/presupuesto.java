4a2634ca9a8b7af6854fe417388b316b0e292258
carlos <carmacias@uan.edu.co>
2016-04-10 20:24:55 -0500

    Actualizadp

--- app/src/main/java/com/example/carlos/proyecto2ab/presupuesto.java
+++ app/src/main/java/com/example/carlos/proyecto2ab/presupuesto.java
@@ -1,7 +1,9 @@
 package com.example.carlos.proyecto2ab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class presupuesto extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class presupuesto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presupuesto);
     }
+
+    public void mensaje (View view) {
+        Intent i = new Intent(this, com.example.carlos.proyecto2ab.MainActivity.class);
+        //i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+
+    }
+
+
+    
+
 }
